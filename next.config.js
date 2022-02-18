9b73087055c4c15c6cea461e6ec0998e5a50ebbb
r9v <r9v>
2022-02-18 19:14:52 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  publicRuntimeConfig: {
+    apiUrl:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/api"
+        : "https://geolocation05e55d7f-e60f-4ea8.herokuapp.com/api",
+  },
+};
+
+module.exports = nextConfig;
